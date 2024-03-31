97d19bd952d20ddd9f17b6506bf0b54aa17b5a8f
tooltime <tooltime@mohawkcollege.ca>
2024-03-31 17:11:02 -0400

    Fixed issue for repeated playing logic

--- rps.py
+++ rps.py
@@ -4,7 +4,12 @@
 
 import random
 
-user_action = input("Enter throw (rock, paper, scissors): ")
+while True:
+    user_action = input("Enter throw (rock, paper, scissors): ")
+    if user_action.lower()=+ 'quit':
+        print(f"Thankyou for Playing")
+        break
+
 ai_action = random.choice(["rock", "paper", "scissors"])
 
 print(f"\nYou chose {user_action}, AI chose {ai_action}.\n")
